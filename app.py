29d292f198b3231e7e2f6c4dbe3df59853f494cd
Alejandra <Perezlluvia730@gmail.com>
2026-03-09 22:02:03 -0600

    Rollback error prueba

--- app.py
+++ app.py
@@ -1,2 +1,4 @@
 def suma (a, b):
     return a + b 
+#def multiplicar(a, b):
+#   return a * b
--- test_app.py
+++ test_app.py
@@ -1,3 +1,7 @@
 from app import suma
 def test_suma():
-    assert suma (2, 2) == 5 #prueba
+    assert suma (2, 2) == 4 #prueba
+
+    #from app import multiplicar
+#def test_multiplicar():
+    #assert multiplicar(3, 3) == 9
