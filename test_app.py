05cbf08f36d455f83d64ce05f950bba0210d33bb
Alejandra <Perezlluvia730@gmail.com>
2026-03-09 22:37:19 -0600

    Added multiplication feature and unit test

--- test_app.py
+++ test_app.py
@@ -2,6 +2,6 @@ from app import suma
 def test_suma():
     assert suma (2, 2) == 4 #prueba
 
-    #from app import multiplicar
-#def test_multiplicar():
-    #assert multiplicar(3, 3) == 9
+    from app import multiplicar
+def test_multiplicar():
+    assert multiplicar(3, 3) == 9
